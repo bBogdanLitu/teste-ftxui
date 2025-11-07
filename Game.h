4003f788f30efb4ff4aa70da438bac281b1c3058
bBogdanLitu <bBogdanLitu@users.noreply.github.com>
2025-11-07 07:40:10 +0200

    Incercare mutare logica din main.cpp in alt loc, dupa folosirea in main

--- Game.h
+++ Game.h
@@ -1,14 +1,16 @@
+#ifndef OOP_GAME_H
+#define OOP_GAME_H
 #include <ftxui/component/component.hpp>       // for Slider, Renderer, Vertical
 #include <ftxui/component/component_base.hpp>  // for ComponentBase
 #include <ftxui/component/screen_interactive.hpp>  // for ScreenInteractive
 #include <ftxui/dom/elements.hpp>  // for separator, operator|, Element, size, text, vbox, xflex, bgcolor, hbox, GREATER_THAN, WIDTH, border, HEIGHT, LESS_THAN
 #include <ftxui/screen/color.hpp>  // for Color
-#include "Game.h"
 
 
+class Game {
+public:
+    void DisplayFTXUI();
+};
 
-int main() {
-    Game game;
-    game.DisplayFTXUI();
-    return 0;
-}
+
+#endif //OOP_GAME_H
